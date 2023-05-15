9f4d8eccca8b2b7b5e85da989ccd64722c790e81
Olivier <olivier.winkler@icloud.com>
2023-05-15 22:27:34 +0200

    feat(layout): add basic layout

---
+++ src/layouts/layout.tsx
@@ -0,0 +1,17 @@
+import { type ReactNode } from 'react';
+import Footer from '~/components/Footer';
+import Header from '~/components/Header';
+import Seo from '~/components/seo/seo';
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      <Seo />
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,30 @@
-import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { type AppProps } from "next/app";
 
 import { api } from "~/utils/api";
 
+import { type NextPage } from "next";
+import { type ReactElement, type ReactNode } from "react";
+import Layout from "~/layouts/layout";
 import "~/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
 };
 
-export default api.withTRPC(MyApp);
+type AppPropsWithLayout<P> = AppProps<P> & {
+  Component: NextPageWithLayout<P>;
+};
+
+const App = ({ Component, pageProps }: AppPropsWithLayout<{ session: Session; }>) => {
+  const getLayout = Component.getLayout ?? (page => <Layout>{page}</Layout>);
+
+  return (
+    <SessionProvider session={pageProps.session}>
+      {getLayout(<Component {...pageProps} />)}
+    </SessionProvider>
+  );
+}
+
+export default api.withTRPC(App);
