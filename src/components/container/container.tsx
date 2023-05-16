22f1cadc06359e1e1b6289f4bec9ce56f631bb63
Olivier <olivier.winkler@icloud.com>
2023-05-16 21:31:57 +0200

    feat(components): add container component

---
+++ src/components/container/container.tsx
@@ -0,0 +1,44 @@
+import { type ReactNode, forwardRef } from 'react'
+
+type Props = {
+  className?: string;
+  children: ReactNode;
+}
+
+const OuterContainer = forwardRef<HTMLDivElement, Props>(function OuterContainer(
+  { className, children, ...props },
+  ref
+) {
+  return (
+    <div ref={ref} className={`sm:px-8 ${className ?? ''}`} {...props} >
+      <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
+    </div >
+  )
+})
+
+const InnerContainer = forwardRef<HTMLDivElement, Props>(function InnerContainer(
+  { className, children, ...props },
+  ref
+) {
+  return (
+    <div
+      ref={ref}
+      className={`relative px-4 sm:px-8 lg:px-12 ${className ?? ''}`}
+      {...props}
+    >
+      <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
+    </div>
+  )
+})
+
+export const Container = forwardRef<HTMLDivElement, Props>(function Container(
+  { children, ...props },
+  ref
+) {
+  return (
+    <OuterContainer ref={ref} {...props}>
+      <InnerContainer>{children}</InnerContainer>
+    </OuterContainer>
+  )
+})
+
