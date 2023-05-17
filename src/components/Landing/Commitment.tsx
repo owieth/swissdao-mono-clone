568bd66e7bfdddb031270a417f4029b5c212f03a
Olivier <olivier.winkler@icloud.com>
2023-05-17 13:48:44 +0200

    feat(commitment): add video teaser

--- src/components/Landing/Commitment.tsx
+++ src/components/Landing/Commitment.tsx
@@ -5,11 +5,13 @@ export default function Committment() {
         <div className="mx-auto flex max-w-7xl flex-col items-center gap-x-8 gap-y-10 px-6 sm:gap-y-8 lg:px-8 xl:flex-row xl:items-stretch">
           <div className="-mt-8 w-full max-w-2xl xl:-mb-8 xl:w-96 xl:flex-none">
             <div className="relative aspect-[2/1] h-full md:-mx-8 xl:mx-0 xl:aspect-auto">
-              <img
+              <video
                 className="absolute inset-0 h-full w-full rounded-2xl bg-gray-800 object-cover shadow-2xl"
-                src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80"
-                alt=""
-              />
+                src="/videos/teaser.mp4"
+                autoPlay
+                loop
+                muted
+              ></video>
             </div>
           </div>
           <div className="w-full max-w-2xl xl:max-w-none xl:flex-auto xl:px-16 xl:py-24">
