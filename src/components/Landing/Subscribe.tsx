7d8b25ab120a505e1faa816eefd062204158ce6f
Olivier <olivier.winkler@icloud.com>
2023-05-27 17:18:34 +0200

    feat(subscribe): add newsletter signup form

--- src/components/Landing/Subscribe.tsx
+++ src/components/Landing/Subscribe.tsx
@@ -18,11 +18,15 @@ export default function Subscribe() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="max-w-2xl text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           <h2 className="inline sm:block">Want community news and updates?</h2>{' '}
-          <p className="inline sm:block">Sign up for our newsletter.</p>
+          <p className="inline sm:block">Sign up for our{' '}
+            <a href="https://forms.gle/YfZhgfgHuNKezRqA7" target="_blank" className="font-semibold text-accent hover:underline">
+              newsletter
+            </a>
+            .</p>
         </div>
         <form className="mt-10 max-w-md">
           <div className="flex gap-x-4">
-            <label htmlFor="email-address" className="sr-only">
+            {/* <label htmlFor="email-address" className="sr-only">
               Email address
             </label>
             <input
@@ -33,13 +37,13 @@ export default function Subscribe() {
               required
               className="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               placeholder="Enter your email"
-            />
-            <button
+            /> */}
+            {/* <button
               type="submit"
               className="flex-none rounded-md bg-background px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Subscribe
-            </button>
+            </button> */}
           </div>
           <p className="mt-4 text-sm leading-6 text-gray-900">
             We care about your data. Read our{' '}
