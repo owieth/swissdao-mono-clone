c20267ce2885c12f3c5a6ab0b27018a23e501a99
Olivier <olivier.winkler@icloud.com>
2023-06-14 07:24:50 +0200

    feat(analytics): run anayltics on every page

--- src/helpers/analytics.tsx
+++ src/helpers/analytics.tsx
@@ -1,19 +1,46 @@
+import { useRouter } from 'next/router';
 import Script from 'next/script';
+import { useEffect } from 'react';
 
-export default function renderGoogleAnaltics() {
+const GA_TRACKING_ID = 'G-3KYP2NRC5M';
+
+const GoogleAnalytics = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      window.gtag('config', GA_TRACKING_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+  }, [router.events]);
   return (
     <>
-      <Script id="google-analytics-script" strategy="afterInteractive" src='https://www.googletagmanager.com/gtag/js?id=G-3KYP2NRC5M' />
-
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-3KYP2NRC5M');
-          `}
-      </Script>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     </>
   );
-}
+};
+
+export default GoogleAnalytics;
