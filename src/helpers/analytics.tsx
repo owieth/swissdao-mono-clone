84ae44ffea2290060ccca6193ee166e08f0e4121
Olivier <olivier.winkler@icloud.com>
2023-06-15 09:10:10 +0200

    feat(analytics): fix undefined window

--- src/helpers/analytics.tsx
+++ src/helpers/analytics.tsx
@@ -12,7 +12,7 @@ const GoogleAnalytics = () => {
       gtag.pageview(url);
     };
 
-    router.events.on('routeChangeComplete', handleRouteChange);
+    if (window) router.events.on('routeChangeComplete', handleRouteChange);
 
     return () => router.events.off('routeChangeComplete', handleRouteChange);
   }, [router.events]);
