97e4b73c8d6a3b02bbccbf881c460f9321dfe54a
Olivier <olivier.winkler@icloud.com>
2023-08-14 14:16:22 +0200

    feat(analytics): fix type

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -2,6 +2,7 @@ export const GA_TRACKING_ID = 'G-3KYP2NRC5M';
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   });
@@ -16,7 +17,7 @@ type GTagEvent = {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent) => {
-  console.log('fire event');
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
