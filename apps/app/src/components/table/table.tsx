96d9e854d241e2f1890196a776ff61a6e4c374fd
Olivier <olivier.winkler@icloud.com>
2023-08-26 21:40:55 +0200

    feat(app): add vercel template boilerplate code

---
+++ apps/app/src/components/table/table.tsx
@@ -0,0 +1,43 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
+  Text
+} from '@tremor/react';
+
+type UserType = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+
+export default function UsersTable({ users }: { users: UserType[] }) {
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableHeaderCell>Name</TableHeaderCell>
+          <TableHeaderCell>Username</TableHeaderCell>
+          <TableHeaderCell>Email</TableHeaderCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {users.map((user) => (
+          <TableRow key={user.id}>
+            <TableCell>{user.name}</TableCell>
+            <TableCell>
+              <Text>{user.username}</Text>
+            </TableCell>
+            <TableCell>
+              <Text>{user.email}</Text>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
