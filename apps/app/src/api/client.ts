be1584e0e6fd0bbdfd4ba068be12b575cace314e
Olivier <olivier.winkler@icloud.com>
2023-08-27 22:51:55 +0200

    feat(app): add notion client

---
+++ apps/app/src/api/client.ts
@@ -0,0 +1,5 @@
+import { Client } from '@notionhq/client';
+
+export const NOTION_CLIENT = new Client({
+  auth: 'secret_OQzVeoJs6FH2U8ydyVa1wjVRqU5rPfPuW3Bnoi44WQ6',
+});
--- apps/app/src/app/members/page.tsx
+++ apps/app/src/app/members/page.tsx
@@ -1,8 +1,24 @@
+import { NOTION_CLIENT } from '@/api/client';
 import Search from '@/components/search/search';
 import UsersTable from '@/components/table/table';
 import { Card, Text, Title } from '@tremor/react';
 
-export default function MembersPage() {
+async function getData() {
+  // const res = await fetch('https://api.example.com/...')
+  // // The return value is *not* serialized
+  // // You can return Date, Map, Set, etc.
+
+  // if (!res.ok) {
+  //   // This will activate the closest `error.js` Error Boundary
+  //   throw new Error('Failed to fetch data')
+  // }
+
+  //return res.json()
+
+  return await NOTION_CLIENT.users.list({ auth: undefined });
+}
+
+export default async function MembersPage() {
   const users = [
     {
       id: 1,
@@ -11,6 +27,10 @@ export default function MembersPage() {
       email: 'max.muster@gmail.com',
     },
   ];
+
+  const data = await getData();
+
+  console.log(data);
 
   return (
     <main className="mx-auto max-w-7xl p-4 md:p-10">
