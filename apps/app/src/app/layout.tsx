3f999d1b442f75dfbe916f396a19c05ac6add360
Olivier <olivier.winkler@icloud.com>
2023-08-28 15:22:10 +0200

    feat(app): copy layout of website

--- apps/app/src/app/layout.tsx
+++ apps/app/src/app/layout.tsx
@@ -1,15 +1,70 @@
 import Navbar from '@/components/navbar/navbar';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 import 'tailwind-config/globals.css';
 import Web3Wrapper from './web3wrapper';
 
-const inter = Inter({ subsets: ['latin'] });
+const avenir = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Avenir-Medium.woff2',
+      weight: '400',
+    },
+    {
+      path: '../../public/fonts/Avenir-Heavy.woff2',
+      weight: '800',
+    },
+  ],
+  variable: '--font-avenir',
+});
+
+const title = 'swissDAO - The Swiss Web3 Builder Community.³';
+const description =
+  'Our goal is to connect builders with projects and join forces to build the new internet.';
+const keywords =
+  'swissDAO, swissdao, daosuisse, suissedao, web3, switzerland, builder, community, decentralized finance, blockchain,';
+const url = 'https://swissdao.space/favicon.ico';
 
 export const metadata: Metadata = {
-  title: 'swissDAO - App',
-  description: 'Crafted by swissDAO',
+  title,
+  description,
+  keywords,
+  openGraph: {
+    title,
+    description,
+    url: 'https://swissdao.space',
+    siteName: 'swissDAO',
+    images: [
+      {
+        url,
+        width: 25,
+        height: 25,
+      },
+    ],
+  },
+  twitter: {
+    title,
+    description,
+    card: 'summary_large_image',
+    creator: '@swissDAOspace',
+    images: {
+      url,
+    },
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 };
 
 export default function RootLayout({
@@ -19,7 +74,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${avenir.variable} font-sans`}>
         <Web3Wrapper>
           <Navbar />
           {children}
