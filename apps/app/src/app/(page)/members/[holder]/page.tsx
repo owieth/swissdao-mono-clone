ff4a41d2cebb874c08e894ae48ac85c1eb3118db
Olivier <olivier.winkler@icloud.com>
2023-09-15 11:36:38 +0200

    feat(app): use avatar on profile page

--- apps/app/src/app/(page)/members/[holder]/page.tsx
+++ apps/app/src/app/(page)/members/[holder]/page.tsx
@@ -3,9 +3,10 @@
 import IconCheck from '@/components/icons/check';
 import IconGithub from '@/components/icons/github';
 import IconUpload from '@/components/icons/upload';
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
 import { useConnectModal } from '@rainbow-me/rainbowkit';
 import Image from 'next/image';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAccount } from 'wagmi';
 
 const profileWidth = 'max-w-5xl mx-auto px-4 sm:px-6 lg:px-8';
@@ -24,6 +25,8 @@ const tabs = [
 ];
 
 export default function Member({ params }: { params: { holder: string } }) {
+  const [edit, setEdit] = useState(false);
+
   const { address } = useAccount();
   const { openConnectModal } = useConnectModal();
 
@@ -41,26 +44,33 @@ export default function Member({ params }: { params: { holder: string } }) {
           className={`${profileWidth} -mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5`}
         >
           <div className="group relative h-24 w-24 overflow-hidden rounded-full sm:h-32 sm:w-32">
-            <button
-              className="absolute z-10 flex h-full w-full items-center justify-center bg-gray-800 bg-opacity-50 transition-all hover:bg-opacity-70"
-              onClick={() =>
-                alert('Image upload has been disabled for demo purposes.')
-              }
-            >
-              <IconUpload className="h-6 w-6 " />
-            </button>
+            {edit && (
+              <button
+                className="absolute z-10 flex h-full w-full items-center justify-center bg-gray-800 bg-opacity-50 transition-all hover:bg-opacity-70"
+                onClick={() =>
+                  alert('Image upload has been disabled for demo purposes.')
+                }
+              >
+                <IconUpload className="h-6 w-6" />
+              </button>
+            )}
 
-            <Image src={user.image} alt={user.name} width={300} height={300} />
+            <Avatar className="h-24 w-24 sm:h-32 sm:w-32">
+              <AvatarImage
+                src="https://avatar.vercel.sh/leerob"
+                alt="@shadcn"
+              />
+            </Avatar>
           </div>
         </div>
       </div>
 
       <div className={`${profileWidth} mt-16`}>
-        <h2 className="font-mono text-2xl font-semibold ">About me</h2>
+        <h2 className="font-mono text-2xl font-semibold">About me</h2>
       </div>
 
       <div className={`${profileWidth} mt-16`}>
-        <h2 className="font-mono text-2xl font-semibold ">My Membership</h2>
+        <h2 className="font-mono text-2xl font-semibold">My Membership</h2>
       </div>
     </div>
   ) : null;
