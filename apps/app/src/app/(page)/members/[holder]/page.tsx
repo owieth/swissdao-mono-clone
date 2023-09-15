8dab0d7524f98e7fbc9a25186942455de11da0c9
Olivier <olivier.winkler@icloud.com>
2023-09-15 11:43:44 +0200

    feat(app): add action buttons to profile view

--- apps/app/src/app/(page)/members/[holder]/page.tsx
+++ apps/app/src/app/(page)/members/[holder]/page.tsx
@@ -8,6 +8,9 @@ import { useConnectModal } from '@rainbow-me/rainbowkit';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { useAccount } from 'wagmi';
+import { CheckIcon, Cross1Icon, Pencil1Icon } from '@radix-ui/react-icons';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import { Button } from '@/components/ui/button';
 
 const profileWidth = 'max-w-5xl mx-auto px-4 sm:px-6 lg:px-8';
 
@@ -37,31 +40,42 @@ export default function Member({ params }: { params: { holder: string } }) {
   }, [address, openConnectModal]);
 
   return address ? (
-    <div className="min-h-screen pb-20">
-      <div>
-        <div className="h-48 w-full bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 lg:h-64" />
-        <div
-          className={`${profileWidth} -mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5`}
-        >
-          <div className="group relative h-24 w-24 overflow-hidden rounded-full sm:h-32 sm:w-32">
-            {edit && (
-              <button
-                className="absolute z-10 flex h-full w-full items-center justify-center bg-gray-800 bg-opacity-50 transition-all hover:bg-opacity-70"
-                onClick={() =>
-                  alert('Image upload has been disabled for demo purposes.')
-                }
-              >
-                <IconUpload className="h-6 w-6" />
-              </button>
-            )}
+    <div className="relative min-h-screen pb-20">
+      <div className="absolute right-4 top-4 flex gap-2 sm:right-6 lg:right-8">
+        <Button size="icon" onClick={() => setEdit(!edit)}>
+          {edit ? (
+            <Cross1Icon className="h-4 w-4" />
+          ) : (
+            <Pencil1Icon className="h-4 w-4" />
+          )}
+        </Button>
 
-            <Avatar className="h-24 w-24 sm:h-32 sm:w-32">
-              <AvatarImage
-                src="https://avatar.vercel.sh/leerob"
-                alt="@shadcn"
-              />
-            </Avatar>
-          </div>
+        {edit && (
+          <Button size="icon">
+            <CheckIcon className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+
+      <div className="h-48 w-full bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 lg:h-64" />
+      <div
+        className={`${profileWidth} -mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5`}
+      >
+        <div className="group relative h-24 w-24 overflow-hidden rounded-full sm:h-32 sm:w-32">
+          {edit && (
+            <button
+              className="absolute z-10 flex h-full w-full items-center justify-center bg-gray-800 bg-opacity-50 transition-all hover:bg-opacity-70"
+              onClick={() =>
+                alert('Image upload has been disabled for demo purposes.')
+              }
+            >
+              <IconUpload className="h-6 w-6" />
+            </button>
+          )}
+
+          <Avatar className="h-24 w-24 sm:h-32 sm:w-32">
+            <AvatarImage src="https://avatar.vercel.sh/leerob" alt="@shadcn" />
+          </Avatar>
         </div>
       </div>
 
