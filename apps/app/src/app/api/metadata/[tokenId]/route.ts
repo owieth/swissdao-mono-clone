f80d293121776ab3783fa1e044bbd51b5ceefb2d
Olivier <olivier.winkler@icloud.com>
2023-09-19 20:53:23 +0200

    feat(app): add other token generation meetadata

--- apps/app/src/app/api/metadata/[tokenId]/route.ts
+++ apps/app/src/app/api/metadata/[tokenId]/route.ts
@@ -1,11 +1,40 @@
 import { TokenStruct } from '@/contracts/contracts';
+import {
+  ACTIVITY_POINT_TOKEN_ID,
+  EVENTS_TOKEN_ID,
+  EXPERIENCE_POINT_TOKEN_ID,
+} from '@/helpers/const';
 import { ETHERS_CONTRACT } from '@/helpers/contracts';
 import { NextResponse } from 'next/server';
 
-export async function GET(
-  request: Request,
-  { params: { tokenId } }: { params: { tokenId: number } }
-) {
+function generateExperience() {
+  return {
+    name: 'XP',
+    description: 'Experience Point - swissDAO',
+    image: `https://owieth-website-app.vercel.app/api/preview/${EXPERIENCE_POINT_TOKEN_ID}`,
+    attributes: [],
+  };
+}
+
+function generateActivity() {
+  return {
+    name: 'AP',
+    description: 'Activity Point - swissDAO',
+    image: `https://owieth-website-app.vercel.app/api/preview/${ACTIVITY_POINT_TOKEN_ID}`,
+    attributes: [],
+  };
+}
+
+function generateEvent() {
+  return {
+    name: 'EP',
+    description: 'Events - swissDAO',
+    image: `https://owieth-website-app.vercel.app/api/preview/${EVENTS_TOKEN_ID}`,
+    attributes: [],
+  };
+}
+
+async function generateMembership(tokenId: string) {
   await ETHERS_CONTRACT.ownerOf(tokenId);
 
   const {
@@ -17,7 +46,7 @@ export async function GET(
     state,
   } = (await ETHERS_CONTRACT.getTokenStructById(tokenId)) as TokenStruct;
 
-  const data = {
+  return {
     name: `Membership #${tokenId}`,
     description: `Membership of ${holder}`,
     image: `https://owieth-website-app.vercel.app/api/preview/${tokenId}`,
@@ -34,6 +63,31 @@ export async function GET(
       { trait_type: 'State', value: Number(state) },
     ],
   };
+}
+
+export async function GET(
+  request: Request,
+  { params: { tokenId } }: { params: { tokenId: string } }
+) {
+  let data;
+
+  switch (tokenId) {
+    case String(EXPERIENCE_POINT_TOKEN_ID):
+      data = generateExperience();
+      break;
+
+    case String(ACTIVITY_POINT_TOKEN_ID):
+      data = generateActivity();
+      break;
+
+    case String(EVENTS_TOKEN_ID):
+      data = generateEvent();
+      break;
+
+    default:
+      data = generateMembership(tokenId);
+      break;
+  }
 
   return NextResponse.json(data);
 }
