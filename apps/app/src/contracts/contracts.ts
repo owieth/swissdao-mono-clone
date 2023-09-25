8dacd0c704889d6902b554308dea44275a399971
Olivier <olivier.winkler@icloud.com>
2023-09-25 19:08:06 +0200

    feat(app): add viem providers to contracts

--- apps/app/src/contracts/contracts.ts
+++ apps/app/src/contracts/contracts.ts
@@ -1,4 +1,11 @@
-import { Address } from 'viem';
+import {
+  Address,
+  PublicClient,
+  createPublicClient,
+  createWalletClient,
+  http,
+} from 'viem';
+import { sepolia } from 'viem/chains';
 import abi from './ABI.json';
 
 export enum TokenState {
@@ -20,6 +27,18 @@ export type TokenStruct = {
 };
 
 export const ALCHEMY_KEY = 'PZYto3ONWzPMqPW6AV70bOzbofx11rQf';
+
+export const ALCHEMY_URL = `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_KEY}`;
+
+export const PUBLIC_PROVIDER = createPublicClient({
+  chain: sepolia,
+  transport: http(ALCHEMY_URL),
+}) as any;
+
+export const WALLET_PROVIDER = createWalletClient({
+  chain: sepolia,
+  transport: http(ALCHEMY_URL),
+});
 
 export const CONTRACT_ADDRESS_SEPOLIA =
   '0xcc46262ACDDd30505071C9232de6d9E63c429091';
