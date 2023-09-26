ce02c3674a5140584166247c28418c0568570c1e
Olivier <olivier.winkler@icloud.com>
2023-09-26 10:42:45 +0200

    feat(app): reset member page

--- apps/app/src/app/(app)/members/page.tsx
+++ apps/app/src/app/(app)/members/page.tsx
@@ -2,9 +2,7 @@
 
 import { NOTION_CLIENT } from '@/api/client';
 import { MembersTable } from '@/components/members-table/members-table';
-import { CONTRACT } from '@/contracts/contracts';
 import { Card, Text, Title } from '@tremor/react';
-import { useContractInfiniteReads } from 'wagmi';
 
 async function getData() {
   // const res = await fetch('https://api.example.com/...')
@@ -22,26 +20,6 @@ async function getData() {
 }
 
 export default function MembersPage() {
-  const { data: memberships, fetchNextPage } = useContractInfiniteReads({
-    cacheKey: 'memberships',
-    contracts(param = 1) {
-      const args = [param] as const;
-      return [{ ...CONTRACT, functionName: 'getTokenStructById', args }] as any;
-    },
-    getNextPageParam: (_, pages) => pages.length + 1,
-  });
-
-  const users = [
-    {
-      id: 1,
-      name: 'Max Muster',
-      username: '@maxmuster',
-      email: 'max.muster@gmail.com',
-    },
-  ];
-
-  console.log(memberships);
-
   return (
     <main className="mx-auto max-w-7xl p-4 md:p-10">
       <Title>Users</Title>
