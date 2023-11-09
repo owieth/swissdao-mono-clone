2b49e914bfce096bbb24a3506c939bd5cb7a8fe8
Olivier <olivier.winkler@icloud.com>
2023-11-09 21:15:15 +0100

    feat(badges): use listem for displaying badges

--- apps/app/src/app/(app)/badges/page.tsx
+++ apps/app/src/app/(app)/badges/page.tsx
@@ -1,19 +1,36 @@
 'use client';
 
-import { MembersTable } from '@/components/members-table/members-table';
-import { CONTRACT } from '@/contracts/contracts';
-import { MemberType } from '@/types/types';
-import { Card, Text, Title } from '@tremor/react';
-import { useContractRead } from 'wagmi';
+import Listitem from '@/components/listitem/listitem';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Text, Title } from '@tremor/react';
 
 export default function BadgesPage() {
+  const BadgeTriggerItem = () => (
+    <div className="flex w-full items-center justify-between">
+      <Avatar className="h-8 w-8">
+        <AvatarImage src={''} alt="@shadcn" />
+        <AvatarFallback></AvatarFallback>
+      </Avatar>
+
+      <span>Badge Name</span>
+
+      <span>Holders</span>
+    </div>
+  );
+
   return (
     <main className="mx-auto max-w-7xl p-4 md:p-10">
       <Title>Badges</Title>
       <Text>A list of all earnable swissDAO Badges</Text>
-      <Card className="mt-6">
-        {/* <MembersTable members={members as MemberType[]} /> */}
-      </Card>
+      <Listitem trigger={<BadgeTriggerItem />}>
+        <div>
+          <h2>About this Badge</h2>
+          <span>Lorem Ipsum</span>
+
+          <h2>About this Badge</h2>
+          <span>Lorem Ipsum</span>
+        </div>
+      </Listitem>
     </main>
   );
 }
