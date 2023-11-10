0f8f85757d0b3c276ecf52d805747db7e387a369
Olivier <olivier.winkler@icloud.com>
2023-11-10 10:46:32 +0100

    feat(badges): use contract read to fetch all badges

--- apps/app/src/app/(app)/badges/page.tsx
+++ apps/app/src/app/(app)/badges/page.tsx
@@ -2,17 +2,26 @@
 
 import Listitem from '@/components/listitem/listitem';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { CONTRACT } from '@/contracts/contracts';
+import { BadgeType } from '@/types/types';
 import { Text, Title } from '@tremor/react';
+import { useContractRead } from 'wagmi';
 
 export default function BadgesPage() {
-  const BadgeTriggerItem = () => (
+  const {
+    data: badges,
+    isError,
+    isLoading,
+  } = useContractRead({ ...CONTRACT, functionName: 'getAllBadges' });
+
+  const BadgeTriggerItem = ({ badge }: { badge: BadgeType }) => (
     <div className="flex w-full items-center justify-between">
       <Avatar className="h-8 w-8">
         <AvatarImage src={''} alt="@shadcn" />
         <AvatarFallback></AvatarFallback>
       </Avatar>
 
-      <span>Badge Name</span>
+      <span>{badge.name}</span>
 
       <span>Holders</span>
     </div>
@@ -22,15 +31,17 @@ export default function BadgesPage() {
     <main className="mx-auto max-w-7xl p-4 md:p-10">
       <Title>Badges</Title>
       <Text>A list of all earnable swissDAO Badges</Text>
-      <Listitem trigger={<BadgeTriggerItem />}>
-        <div>
-          <h2>About this Badge</h2>
-          <span>Lorem Ipsum</span>
+      {(badges as BadgeType[])?.map((badge, i) => (
+        <Listitem key={i} trigger={<BadgeTriggerItem badge={badge} />}>
+          <div>
+            <h2>About this Badge</h2>
+            <span>Lorem Ipsum</span>
 
-          <h2>About this Badge</h2>
-          <span>Lorem Ipsum</span>
-        </div>
-      </Listitem>
+            <h2>About this Badge</h2>
+            <span>Lorem Ipsum</span>
+          </div>
+        </Listitem>
+      ))}
     </main>
   );
 }
