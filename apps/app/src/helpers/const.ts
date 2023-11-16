dccddd3abdcc18f6ac788fc274badfb777220fb7
Olivier <olivier.winkler@icloud.com>
2023-11-16 19:18:59 +0100

    feat(const): add tokenid ranges as consts

--- apps/app/src/helpers/const.ts
+++ apps/app/src/helpers/const.ts
@@ -1,3 +1,9 @@
 export const EXPERIENCE_POINT_TOKEN_ID = 1;
 export const ACTIVITY_POINT_TOKEN_ID = 2;
 export const EVENTS_TOKEN_ID = 3;
+
+// Token ID Ranges
+export const GUILD_INITIAL_COUNTER = 50;
+export const BADGE_INITIAL_COUNTER = 100;
+export const EVENT_INITIAL_COUNTER = 1000;
+export const MEMBERSHIP_INITIAL_COUNTER = 10_000;
