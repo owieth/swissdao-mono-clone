03618b7d1f4e51954f8d24f4772159dee9edec14
Olivier <olivier.winkler@icloud.com>
2023-11-16 20:55:18 +0100

    feat(guilds): add guilds table to guilds page

--- apps/app/src/app/(app)/guilds/page.tsx
+++ apps/app/src/app/(app)/guilds/page.tsx
@@ -1,11 +1,64 @@
+// 'use client';
+
+// import Listitem from '@/components/listitem/listitem';
+// import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+// import { CONTRACT } from '@/contracts/contracts';
+// import { BADGE_INITIAL_COUNTER, GUILD_INITIAL_COUNTER } from '@/helpers/const';
+// import { TokenType } from '@/types/types';
+// import { Text, Title } from '@tremor/react';
+// import { useContractRead } from 'wagmi';
+
+// export default function BadgesPage() {
+//   const {
+//     data: guilds,
+//     isError,
+//     isLoading,
+//   } = useContractRead({ ...CONTRACT, functionName: 'getAllBadges' });
+
+//   const BadgeTriggerItem = ({ badge }: { badge: TokenType }) => (
+//     <div className="flex w-full items-center justify-between">
+//       <Avatar className="h-8 w-8">
+//         <AvatarImage src={''} alt="@shadcn" />
+//         <AvatarFallback></AvatarFallback>
+//       </Avatar>
+
+//       <span>{badge.name}</span>
+
+//       <span>Holders</span>
+//     </div>
+//   );
+
+//   return (
+//     <main className="mx-auto max-w-7xl p-4 md:p-10">
+//       <Title>Guilds</Title>
+//       <Text>A list of all swissDAO Guilds</Text>
+//       {(guilds as TokenType[])
+//         ?.filter(
+//           ({ tokenId }) =>
+//             tokenId >= GUILD_INITIAL_COUNTER && tokenId < BADGE_INITIAL_COUNTER
+//         )
+//         .map((badge, i) => (
+//           <Listitem key={i} trigger={<BadgeTriggerItem badge={badge} />}>
+//             <div>
+//               <h2>About this Badge</h2>
+//               <span>Lorem Ipsum</span>
+
+//               <h2>About this Badge</h2>
+//               <span>Lorem Ipsum</span>
+//             </div>
+//           </Listitem>
+//         ))}
+//     </main>
+//   );
+// }
+
 'use client';
 
-import Listitem from '@/components/listitem/listitem';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { GuildsTable } from '@/components/tables/guilds-table';
 import { CONTRACT } from '@/contracts/contracts';
 import { BADGE_INITIAL_COUNTER, GUILD_INITIAL_COUNTER } from '@/helpers/const';
 import { TokenType } from '@/types/types';
-import { Text, Title } from '@tremor/react';
+import { Card, Text, Title } from '@tremor/react';
 import { useContractRead } from 'wagmi';
 
 export default function BadgesPage() {
@@ -15,39 +68,19 @@ export default function BadgesPage() {
     isLoading,
   } = useContractRead({ ...CONTRACT, functionName: 'getAllBadges' });
 
-  const BadgeTriggerItem = ({ badge }: { badge: TokenType }) => (
-    <div className="flex w-full items-center justify-between">
-      <Avatar className="h-8 w-8">
-        <AvatarImage src={''} alt="@shadcn" />
-        <AvatarFallback></AvatarFallback>
-      </Avatar>
-
-      <span>{badge.name}</span>
-
-      <span>Holders</span>
-    </div>
-  );
-
   return (
     <main className="mx-auto max-w-7xl p-4 md:p-10">
-      <Title>Guilds</Title>
-      <Text>A list of all swissDAO Guilds</Text>
-      {(guilds as TokenType[])
-        ?.filter(
-          ({ tokenId }) =>
-            tokenId >= GUILD_INITIAL_COUNTER && tokenId < BADGE_INITIAL_COUNTER
-        )
-        .map((badge, i) => (
-          <Listitem key={i} trigger={<BadgeTriggerItem badge={badge} />}>
-            <div>
-              <h2>About this Badge</h2>
-              <span>Lorem Ipsum</span>
-
-              <h2>About this Badge</h2>
-              <span>Lorem Ipsum</span>
-            </div>
-          </Listitem>
-        ))}
+      <Title>Users</Title>
+      <Text>A list of all registered Members</Text>
+      <Card className="mt-6">
+        <GuildsTable
+          guilds={(guilds as TokenType[])?.filter(
+            ({ tokenId }) =>
+              tokenId >= GUILD_INITIAL_COUNTER &&
+              tokenId < BADGE_INITIAL_COUNTER
+          )}
+        />
+      </Card>
     </main>
   );
 }
