35835e09686c8c24291d5fa262f464e1d185c4c2
Olivier <olivier.winkler@icloud.com>
2023-11-16 21:22:50 +0100

    feat(members): remove console.log

--- apps/app/src/app/(app)/members/page.tsx
+++ apps/app/src/app/(app)/members/page.tsx
@@ -29,7 +29,6 @@ export default function MembersPage() {
     isLoading,
   } = useContractRead({ ...CONTRACT, functionName: 'getAllMembers' });
 
-  console.log(members);
   return (
     <main className="mx-auto max-w-7xl p-4 md:p-10">
       <Title>Users</Title>
