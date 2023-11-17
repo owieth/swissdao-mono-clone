8da735c804152ae15cc98540f7b3f89a494d161e
Olivier <olivier.winkler@icloud.com>
2023-11-17 20:27:34 +0100

    feat(profile): add dialog to profile

--- apps/app/src/app/(app)/members/[holder]/page.tsx
+++ apps/app/src/app/(app)/members/[holder]/page.tsx
@@ -1,9 +1,16 @@
 'use client';
 
+import { UpdateMembershipForm } from '@/components/forms/membership';
 import IconUpload from '@/components/icons/upload';
 import { MemberStats } from '@/components/member-stats/member-stats';
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
 import { CONTRACT } from '@/contracts/contracts';
 import { getGradient } from '@/helpers/gradient';
 import { MemberType } from '@/types/types';
@@ -42,68 +49,81 @@ export default function Member() {
   }, [address, openConnectModal]);
 
   return member ? (
-    <div className="relative min-h-screen pb-20">
-      {isProfileOwner && (
-        <div className="absolute right-4 top-4 flex gap-2 sm:right-6 lg:right-8">
-          <Button size="icon" onClick={() => setEdit(!edit)}>
-            {edit ? (
-              <Cross1Icon className="h-4 w-4" />
-            ) : (
-              <Pencil1Icon className="h-4 w-4" />
-            )}
-          </Button>
+    <>
+      <Dialog open={edit} onOpenChange={setEdit}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit Membership</DialogTitle>
+          </DialogHeader>
 
-          {edit && (
-            <Button size="icon">
-              <CheckIcon className="h-4 w-4" />
+          {/* <UpdateMembershipForm /> */}
+        </DialogContent>
+      </Dialog>
+
+      <div className="relative min-h-screen pb-20">
+        {isProfileOwner && (
+          <div className="absolute right-4 top-4 flex gap-2 sm:right-6 lg:right-8">
+            <Button size="icon" onClick={() => setEdit(!edit)}>
+              {edit ? (
+                <Cross1Icon className="h-4 w-4" />
+              ) : (
+                <Pencil1Icon className="h-4 w-4" />
+              )}
             </Button>
-          )}
-        </div>
-      )}
 
-      {/* <div className="h-48 w-full bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 lg:h-64" /> */}
+            {edit && (
+              <Button size="icon">
+                <CheckIcon className="h-4 w-4" />
+              </Button>
+            )}
+          </div>
+        )}
 
-      <div
-        className={`h-48 w-full lg:h-64 
+        <div
+          className={`h-48 w-full lg:h-64 
           ${getGradient((member as any).membership.nickname)}`}
-      />
+        />
 
-      <div
-        className={`${profileWidth} -mt-12 items-center sm:-mt-16 sm:flex sm:space-x-5`}
-      >
-        <div className="group relative h-24 w-24 overflow-hidden rounded-full sm:h-32 sm:w-32">
-          {edit && (
-            <button
-              className="absolute z-10 flex h-full w-full items-center justify-center bg-gray-800 bg-opacity-50 transition-all hover:bg-opacity-70"
-              onClick={() =>
-                alert('Image upload has been disabled for demo purposes.')
-              }
-            >
-              <IconUpload className="h-6 w-6" />
-            </button>
-          )}
+        <div
+          className={`${profileWidth} -mt-12 items-center sm:-mt-16 sm:flex sm:space-x-5`}
+        >
+          <div className="group relative h-24 w-24 overflow-hidden rounded-full sm:h-32 sm:w-32">
+            {edit && (
+              <button
+                className="absolute z-10 flex h-full w-full items-center justify-center bg-gray-800 bg-opacity-50 transition-all hover:bg-opacity-70"
+                onClick={() =>
+                  alert('Image upload has been disabled for demo purposes.')
+                }
+              >
+                <IconUpload className="h-6 w-6" />
+              </button>
+            )}
 
-          <Avatar className="h-24 w-24 sm:h-32 sm:w-32">
-            <AvatarImage src="https://avatar.vercel.sh/leerob" alt="@shadcn" />
-          </Avatar>
+            <Avatar className="h-24 w-24 sm:h-32 sm:w-32">
+              <AvatarImage
+                src="https://avatar.vercel.sh/leerob"
+                alt="@shadcn"
+              />
+            </Avatar>
+          </div>
+
+          <div className="mt-16">
+            <h1 className="truncate text-2xl font-semibold text-black">
+              {(member as MemberType).membership.nickname}
+            </h1>
+          </div>
         </div>
 
-        <div className="mt-16">
-          <h1 className="truncate text-2xl font-semibold text-black">
-            {(member as MemberType).membership.nickname}
-          </h1>
-        </div>
-      </div>
-
-      {/* <div className={`${profileWidth} mt-16`}>
+        {/* <div className={`${profileWidth} mt-16`}>
         <h2 className="font-mono text-2xl font-semibold">About me</h2>
         {edit ? <Textarea>{userBio}</Textarea> : <span>{userBio}</span>}
       </div> */}
 
-      <div className={`${profileWidth} mt-16`}>
-        <h2 className="font-mono text-2xl font-semibold">My Membership</h2>
-        <MemberStats />
+        <div className={`${profileWidth} mt-16`}>
+          <h2 className="font-mono text-2xl font-semibold">My Membership</h2>
+          <MemberStats />
+        </div>
       </div>
-    </div>
+    </>
   ) : null;
 }
