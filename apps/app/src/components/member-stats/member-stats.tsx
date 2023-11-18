a82a3aaa5059e560b83daa4dd2c78ce57054163b
Olivier <olivier.winkler@icloud.com>
2023-11-18 11:58:02 +0100

    feat(members-stats): use other icons

--- apps/app/src/components/member-stats/member-stats.tsx
+++ apps/app/src/components/member-stats/member-stats.tsx
@@ -1,6 +1,12 @@
 import { LineChart } from '@tremor/react';
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
 import { MemberType } from '@/types/types';
+import {
+  IdCardIcon,
+  LapTimerIcon,
+  PersonIcon,
+  UpdateIcon,
+} from '@radix-ui/react-icons';
 
 const chartdata = [
   {
@@ -45,18 +51,7 @@ export function MemberStats({ member }: Props) {
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Total Badges</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="text-muted-foreground h-4 w-4"
-            >
-              <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-            </svg>
+            <IdCardIcon />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">{member.badges.length}</div>
@@ -68,20 +63,7 @@ export function MemberStats({ member }: Props) {
             <CardTitle className="text-sm font-medium">
               Activity Points
             </CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="text-muted-foreground h-4 w-4"
-            >
-              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-              <circle cx="9" cy="7" r="4" />
-              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-            </svg>
+            <LapTimerIcon />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">
@@ -95,19 +77,7 @@ export function MemberStats({ member }: Props) {
             <CardTitle className="text-sm font-medium">
               Experience Points
             </CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="text-muted-foreground h-4 w-4"
-            >
-              <rect width="20" height="14" x="2" y="5" rx="2" />
-              <path d="M2 10h20" />
-            </svg>
+            <PersonIcon />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">
@@ -120,18 +90,7 @@ export function MemberStats({ member }: Props) {
             <CardTitle className="text-sm font-medium">
               Attended Events
             </CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="text-muted-foreground h-4 w-4"
-            >
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-            </svg>
+            <UpdateIcon />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">
