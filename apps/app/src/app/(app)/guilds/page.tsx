ca4c4d8c88a846952678cbf01abd25f6aff7d34d
Olivier <olivier.winkler@icloud.com>
2024-01-21 12:24:58 +0100

    feat(guilds): rework guilds overview page

--- apps/app/src/app/(app)/guilds/page.tsx
+++ apps/app/src/app/(app)/guilds/page.tsx
@@ -54,15 +54,30 @@
 
 'use client';
 
-import { GuildsTable } from '@/components/tables/guilds-table';
 import { MembershipContext } from '@/contexts/membership';
 import { CONTRACT } from '@/contracts/contracts';
 import { BADGE_INITIAL_COUNTER, GUILD_INITIAL_COUNTER } from '@/helpers/const';
-import { GuildType, MemberType, MembershipType } from '@/types/types';
-import { Card, Text, Title } from '@tremor/react';
+import { GuildType, MemberType } from '@/types/types';
+import { Text, Title } from '@tremor/react';
 import { prepareWriteContract, writeContract } from '@wagmi/core';
+import Link from 'next/link';
 import { useContext } from 'react';
 import { useContractRead, useContractReads } from 'wagmi';
+
+function CSSstring(string: string) {
+  const css_json = `{"${string
+    .replace(/; /g, '", "')
+    .replace(/: /g, '": "')
+    .replace(';', '')}"}`;
+
+  const obj = JSON.parse(css_json);
+
+  const keyValues = Object.keys(obj).map(key => {
+    var camelCased = key.replace(/-[a-z]/g, g => g[1].toUpperCase());
+    return { [camelCased]: obj[key] };
+  });
+  return Object.assign({}, ...keyValues);
+}
 
 export default function BadgesPage() {
   const { membership } = useContext(MembershipContext);
@@ -102,28 +117,66 @@ export default function BadgesPage() {
 
   return (
     <main className="mx-auto max-w-7xl p-4 md:p-10">
-      <Title>Users</Title>
-      <Text>A list of all registered Members</Text>
-      <Card className="mt-6">
-        <GuildsTable
-          guilds={(guilds as GuildType[])
-            ?.filter(
-              ({ badge }) =>
-                badge.tokenId >= GUILD_INITIAL_COUNTER &&
-                badge.tokenId < BADGE_INITIAL_COUNTER
-            )
-            .map(guild => {
-              return {
-                ...guild,
-                members:
-                  members?.filter(member =>
-                    guild.holders.includes(member.holder)
-                  ) || []
-              };
-            })}
-          onJoinGuild={onJoinGuild}
-        />
-      </Card>
+      <Title>Guilds</Title>
+      <Text>All swissDAO Guilds</Text>
+
+      <div className="mt-10 grid w-full grid-cols-1 gap-8 px-10 sm:grid-cols-2 md:px-20 lg:mt-20 xl:grid-cols-3">
+        {(guilds as GuildType[])
+          ?.filter(
+            ({ badge }) =>
+              badge.tokenId >= GUILD_INITIAL_COUNTER &&
+              badge.tokenId < BADGE_INITIAL_COUNTER
+          )
+          .map(guild => {
+            return {
+              ...guild,
+              members:
+                members?.filter(member =>
+                  guild.holders.includes(member.holder)
+                ) || []
+            };
+          })
+          .map((guild, i) => (
+            <Link href={`/guilds/${guild.badge.tokenId}`} key={i}>
+              <div className="ease rounded-2xl border-2 border-gray-100 bg-white shadow transition-all duration-200 hover:-translate-y-1 hover:shadow-xl">
+                <div className="overflow-hidden rounded-t-2xl">
+                  <span
+                    style={CSSstring(
+                      'box-sizing: border-box; display: block; overflow: hidden; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative;'
+                    )}
+                  >
+                    <span
+                      style={CSSstring(
+                        'box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 80% 0px 0px;'
+                      )}
+                    ></span>
+                    <img
+                      alt="Developer DAO"
+                      sizes="100vw"
+                      src="/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdaojones%2Fimage%2Fupload%2FCleanShot_2021-11-24_at_04.08.33_pxl0kp.png&amp;w=3840&amp;q=75"
+                      decoding="async"
+                      data-nimg="responsive"
+                      className="scale-100 blur-0 grayscale-0 duration-700 ease-in-out"
+                      style={CSSstring(
+                        'position: absolute; inset: 0px; box-sizing: border-box; padding: 0px; border: none; margin: auto; display: block; width: 0px; height: 0px; min-width: 100%; max-width: 100%; min-height: 100%; max-height: 100%; object-fit: cover;'
+                      )}
+                    />
+                  </span>
+                </div>
+
+                <div className="h-36 px-5 py-6">
+                  <h3 className="font-cal my-0 truncate text-2xl font-bold tracking-wide">
+                    {guild.badge.name}
+                  </h3>
+                  <p className="mt-3 line-clamp-2 text-base font-normal italic leading-snug text-gray-800">
+                    Build web3 with frens 🤝
+                  </p>
+                </div>
+              </div>
+            </Link>
+          ))}
+      </div>
+      {/* </Card> */}
     </main>
   );
 }
