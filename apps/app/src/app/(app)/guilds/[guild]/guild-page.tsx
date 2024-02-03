5b537a614c49207e6cba5ac5c417f77570f62d3e
Olivier <olivier.winkler@icloud.com>
2024-02-03 13:48:44 +0100

    feat(guild): add guild server rendered page

---
+++ apps/app/src/app/(app)/guilds/[guild]/guild-page.tsx
@@ -0,0 +1,5 @@
+import { GuildType } from '@/types/types';
+
+export default function GuildPage({ guild }: { guild: GuildType }) {
+  return <div>guild</div>;
+}
--- apps/app/src/app/(app)/guilds/[guild]/page.tsx
+++ apps/app/src/app/(app)/guilds/[guild]/page.tsx
@@ -1,5 +1,43 @@
-'use client';
+import { fetchSubgraph } from '@/api/subgraph';
+import { Metadata } from 'next';
+import GuildPage from './guild-page';
 
-export default function Guild() {
-  return <div>Guild</div>;
+export const metadata: Metadata = {
+  title: 'Member'
+};
+
+async function getData(id: string) {
+  const members = await fetchSubgraph(`
+    query {
+      guild(id:${id}) {
+        id
+        name
+        imageUri
+        holder {
+          id
+          tokenID
+          profileImageUri
+          nickname
+          holder
+          joined_At
+          minted_At
+          experiencePoints
+          activityPoints
+          attendedEvents
+        }
+      }
+    }
+  `);
+
+  return members;
+}
+
+export default async function Page({
+  params: { id }
+}: {
+  params: { id: string };
+}) {
+  const { guild } = await getData(id);
+
+  return <GuildPage guild={guild} />;
 }
