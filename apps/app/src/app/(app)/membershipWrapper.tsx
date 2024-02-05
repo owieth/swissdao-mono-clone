0f6605a3c53cc0a55b4d219a5b066ea5a8e8d87b
Olivier <olivier.winkler@icloud.com>
2024-02-05 19:33:03 +0100

    feat(wrapper): add isAdmin property to query

--- apps/app/src/app/(app)/membershipWrapper.tsx
+++ apps/app/src/app/(app)/membershipWrapper.tsx
@@ -30,6 +30,7 @@ export default function MembershipWrapper({
             experiencePoints
             activityPoints
             attendedEvents
+            isAdmin
           }
         }
       `)) as { memberships: MembershipType[] };
