45dd8359db6023609e070dfe3ca162ce3c61d645
Olivier <olivier.winkler@icloud.com>
2024-02-08 13:49:08 +0100

    feat(activity-log): add time based refetch

--- apps/app/src/components/dashboard/activity-log.tsx
+++ apps/app/src/components/dashboard/activity-log.tsx
@@ -1,13 +1,19 @@
 import { fetchSubgraph } from '@/api/subgraph';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { TransactionType } from '@/types/types';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { Icons } from '../ui/icons';
 
 export default function ActivityLog() {
   const [transactions, setTransactions] = useState<TransactionType[]>([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const timerRef = useRef<NodeJS.Timeout>();
 
   useEffect(() => {
     const fetchTransactions = async () => {
+      setIsFetching(true);
+
       const { transactions: _transactions } = (await fetchSubgraph(`
         query {
           transactions(orderBy:timestamp, orderDirection:desc) {
@@ -30,13 +36,24 @@ export default function ActivityLog() {
       `)) as { transactions: TransactionType[] };
 
       setTransactions(_transactions);
+      setIsFetching(false);
     };
 
     fetchTransactions();
+
+    timerRef.current = setInterval(() => fetchTransactions(), 10000);
+
+    return () => clearTimeout(timerRef.current);
   }, []);
 
   return (
     <div className="space-y-8">
+      {isFetching && (
+        <div className="flex justify-center">
+          <Icons.spinner className="animate-spin" />
+        </div>
+      )}
+
       {transactions.map((transaction, i) => (
         <div className="flex items-center" key={i}>
           <Avatar className="h-9 w-9">
