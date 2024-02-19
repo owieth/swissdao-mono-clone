9cef69a0e5568ba47f6a13600ea1c2127262a02e
Olivier <olivier.winkler@icloud.com>
2024-02-19 19:49:03 +0100

    feat(activity-log): refactor transactions into table

--- apps/app/src/components/dashboard/activity-log.tsx
+++ apps/app/src/components/dashboard/activity-log.tsx
@@ -1,13 +1,56 @@
+'use client';
+
 import { fetchSubgraph } from '@/api/subgraph';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components//ui/table';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { TransactionType } from '@/types/types';
-import { useState, useEffect, useRef } from 'react';
-import { Icons } from '../ui/icons';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { Input } from '@/components/ui/input';
+import {
+  MembershipType,
+  TransactionType,
+  TransactionTypeEnum
+} from '@/types/types';
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable
+} from '@tanstack/react-table';
+import { ArrowUpRight, MoreHorizontal } from 'lucide-react';
 import Link from 'next/link';
+import * as React from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Icons } from '../ui/icons';
+import { shortenBytes32 } from '@/helpers/format';
 
 export default function ActivityLog() {
-  const [transactions, setTransactions] = useState<TransactionType[]>([]);
-  const [isFetching, setIsFetching] = useState(false);
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+
+  const [transactions, setTransactions] = React.useState<TransactionType[]>([]);
+  const [isFetching, setIsFetching] = React.useState(false);
 
   const timerRef = useRef<NodeJS.Timeout>();
 
@@ -47,46 +90,188 @@ export default function ActivityLog() {
     return () => clearTimeout(timerRef.current);
   }, []);
 
-  return (
-    <div className="space-y-8">
-      {isFetching && (
-        <div className="flex justify-center">
-          <Icons.spinner className="animate-spin" />
-        </div>
-      )}
+  const columns: ColumnDef<TransactionType>[] = [
+    {
+      accessorKey: 'action',
+      header: 'Action',
+      cell: ({ row }) => <div>{row.original.type}</div>
+    },
+    {
+      accessorKey: 'member',
+      header: 'Member',
+      cell: ({ row }) => {
+        let member: MembershipType;
 
-      {transactions.map((transaction, i) => (
-        <div className="flex items-center" key={i}>
-          <Avatar className="h-9 w-9">
-            <AvatarImage
-              src={
-                transaction.from?.profileImageUri ||
-                transaction.to?.profileImageUri
-              }
-              alt="Avatar"
-            />
-            <AvatarFallback>
-              {transaction.from?.nickname || transaction.to?.nickname}
-            </AvatarFallback>
-          </Avatar>
-          <div className="ml-4 space-y-1">
-            <p className="text-sm font-medium leading-none">
-              {transaction.from?.nickname || transaction.to?.nickname}
-            </p>
-            <p className="text-muted-foreground text-sm">
-              {transaction.from?.holder || transaction.to?.holder}
-            </p>
+        switch (row.original.type) {
+          case TransactionTypeEnum.MEMBERSHIP_MINT:
+          case TransactionTypeEnum.MEMBERSHIP_EDIT:
+          case TransactionTypeEnum.TOKEN_MINT:
+          case TransactionTypeEnum.BADGE_MINT:
+          case TransactionTypeEnum.GUILD_JOIN:
+            member = row.original.to;
+            break;
+
+          case TransactionTypeEnum.MEMBERSHIP_BURN:
+          case TransactionTypeEnum.TOKEN_BURN:
+          case TransactionTypeEnum.BADGE_BURN:
+          case TransactionTypeEnum.GUILD_LEAVE:
+            member = row.original.from;
+            break;
+
+          default:
+            // TODO check
+            member = row.original.to;
+            break;
+        }
+
+        return (
+          <div className="flex items-center gap-4">
+            <Avatar className="h-8 w-8">
+              <AvatarImage src={member.profileImageUri} alt="@shadcn" />
+              <AvatarFallback>{member.nickname}</AvatarFallback>
+            </Avatar>
+            {member.nickname}
           </div>
-          <div className="ml-auto font-medium">
+        );
+      }
+    },
+    {
+      accessorKey: 'txHash',
+      header: 'Transaction Hash',
+      cell: ({ row }) => {
+        const { txHash } = row.original;
+
+        return (
+          <div className="group">
             <Link
-              href={`https://sepolia-optimism.etherscan.io/tx/${transaction.txHash}`}
+              href={`https://sepolia-optimism.etherscan.io/tx/${txHash}`}
               target="_blank"
+              className="flex items-center group-hover:underline"
             >
-              {transaction.type}
+              {shortenBytes32(txHash)}
+              <ArrowUpRight className="size-4 ml-2 shrink-0 opacity-50 group-hover:opacity-100" />
             </Link>
           </div>
+        );
+      }
+    },
+    {
+      id: 'actions',
+      enableHiding: false,
+      cell: ({ row }) => {
+        const { tokenID } = row.original;
+
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link href={`/members/${tokenID}`}>View profile</Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      }
+    }
+  ];
+
+  const table = useReactTable({
+    data: transactions || [],
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting,
+      columnFilters
+    }
+  });
+
+  return (
+    <div className="w-full">
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map(headerGroup => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map(header => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {isFetching && (
+              <div className="flex justify-center">
+                <Icons.spinner className="animate-spin" />
+              </div>
+            )}
+
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map(row => (
+                <TableRow key={row.id}>
+                  {row.getVisibleCells().map(cell => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
         </div>
-      ))}
+      </div>
     </div>
   );
 }
