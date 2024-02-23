b733363f12329e009bf86cd314103b591cd1bb84
Olivier <olivier.winkler@icloud.com>
2024-02-23 20:36:55 +0100

    feat(activity-log): fix member undefined

--- apps/app/src/components/dashboard/activity-log.tsx
+++ apps/app/src/components/dashboard/activity-log.tsx
@@ -126,11 +126,15 @@ export default function ActivityLog() {
 
         return (
           <div className="flex items-center gap-4">
-            <Avatar className="h-8 w-8">
-              <AvatarImage src={member.profileImageUri} alt="@shadcn" />
-              <AvatarFallback>{member.nickname}</AvatarFallback>
-            </Avatar>
-            {member.nickname}
+            {member && (
+              <>
+                <Avatar className="h-8 w-8">
+                  <AvatarImage src={member.profileImageUri} alt="@shadcn" />
+                  <AvatarFallback>{member.nickname}</AvatarFallback>
+                </Avatar>
+                {member.nickname}
+              </>
+            )}
           </div>
         );
       }
